8ae3b12c394b22d53911acb99da59f4b4bdf1b6b
Robert Reed <bobby@Robert-Reeds-Mac-Pro.local>
2015-10-16 14:24:40 -0700

    First commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/jhawes/pen/ujdgK.
+
+ Outputs the coordinates (longitude, latitude) for Google Maps whenever the Polygon is moved or re-shaped. Recently added a 'Copy to Clipboard' button and had data output into a textarea for easier copying & pasting
+
+I originally built this to build map outlines for neighborhoods, villages, districts and counties - specifically for real estate purposes.
---
+++ js/index.js
@@ -0,0 +1,50 @@
+//var myPolygon;
+function initialize() {
+  // Map Center
+  var myLatLng = new google.maps.LatLng(33.5190755, -111.9253654);
+  // General Options
+  var mapOptions = {
+    zoom: 12,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.RoadMap
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+  // Polygon Coordinates
+  var triangleCoords = [
+    new google.maps.LatLng(33.5362475, -111.9267386),
+    new google.maps.LatLng(33.5104882, -111.9627875),
+    new google.maps.LatLng(33.5004686, -111.9027061)
+  ];
+  // Styling & Controls
+  myPolygon = new google.maps.Polygon({
+    paths: triangleCoords,
+    draggable: true, // turn off if it gets annoying
+    editable: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  myPolygon.setMap(map);
+  //google.maps.event.addListener(myPolygon, "dragend", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "insert_at", getPolygonCoords);
+  //google.maps.event.addListener(myPolygon.getPath(), "remove_at", getPolygonCoords);
+  google.maps.event.addListener(myPolygon.getPath(), "set_at", getPolygonCoords);
+}
+
+//Display Coordinates below map
+function getPolygonCoords() {
+  var len = myPolygon.getPath().getLength();
+  var htmlStr = "";
+  for (var i = 0; i < len; i++) {
+    htmlStr += "new google.maps.LatLng(" + myPolygon.getPath().getAt(i).toUrlValue(5) + "), ";
+    //Use this one instead if you want to get rid of the wrap > new google.maps.LatLng(),
+    //htmlStr += "" + myPolygon.getPath().getAt(i).toUrlValue(5);
+  }
+  document.getElementById('info').innerHTML = htmlStr;
+}
+function copyToClipboard(text) {
+  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
